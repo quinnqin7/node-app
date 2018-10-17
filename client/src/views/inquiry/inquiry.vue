66d61b5ca99a046d067110c7b3f0c279a39d34b9
QinNan <quinnqin7@gmail.com>
2018-10-17 10:13:34 +0800

    all

---
+++ client/src/views/inquiry/inquiry.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        问诊
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:"inquiry",
+    data(){
+
+    },
+    component:{
+       
+    }
+}
+</script>
+
+<style>
+
+</style>
+
