1237519d29645acf1f739f874f83689997dea876
afoolman <jiakunguo@icloud.com>
2018-11-03 04:02:46 +0800

    Sendmail

--- web/config/index.js
+++ web/config/index.js
@@ -19,7 +19,7 @@ module.exports = {
         }}},
 
     // Various Dev Server settings
-    host: '120.110.93.189', // can be overwritten by process.env.HOST
+    host: '66,112,214,13', // can be overwritten by process.env.HOST
     port: 9528, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
