7f301f591c1a91fa687b50215b0af6076aa0d1ba
afoolman <jiakunguo@icloud.com>
2018-11-03 08:58:26 +0800

    Sendmail

--- web/config/dev.env.js
+++ web/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://med.4843.fun:80"',
+  BASE_API: '"http://med.4843.fun:9999"',
 })
--- web/config/index.js
+++ web/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'med.4843.fun', // can be overwritten by process.env.HOST
-    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 9999, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: false,
